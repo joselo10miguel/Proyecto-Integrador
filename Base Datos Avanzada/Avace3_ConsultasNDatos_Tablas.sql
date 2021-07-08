93ea48f5034466fe77fce62c6ccd6c035be3f8ef
HernanSanchez <hernan.alejo.2001@gmail.com>
2021-07-08 11:12:12 -0500

    Avanze3

---
+++ Base Datos Avanzada/Avace3_ConsultasNDatos_Tablas.sql
@@ -0,0 +1,23 @@
+-- Avance 3
+use covi19;
+-- cantidad de registros que tiene la tabla casosconfirmados 
+select count(*)
+from casosconfirmados; 
+-- cantidad de registros que tiene la tabla empresa_vacuna_covid 
+select count(*)
+from empresa_vacuna_covid;
+-- cantidad de registros que tiene la tabla hospitalizados 
+select count(*)
+from hospitalizados;
+-- cantidad de registros que tiene la tabla muertescausadelcovid19 
+select count(*)
+from muertescausadelcovid19 ;
+-- cantidad de registros que tiene la tabla tablaprincipal 
+select count(*)
+from tablaprincipal;
+-- cantidad de registros que tiene la tabla vacunados
+select count(*)
+from vacunados;
+-- cantidad de registros que tiene la tabla variablessocioeconomicas
+select count(*)
+from variablessocioeconomicas;
